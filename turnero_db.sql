c7bb407524d83b7f7e039047260d1605c5da2c77
Saibuerns <ema_seiguer@hotmail.com>
2018-04-06 02:03:59 -0300

    Actualize la bd

--- turnero_db.sql
+++ turnero_db.sql
@@ -5,9 +5,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema turnero
 -- -----------------------------------------------------
 
@@ -29,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `turnero`.`entity` (
   `deleted_at` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 0
+AUTO_INCREMENT = 6
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -52,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `turnero`.`sub_entity` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 0
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -109,9 +106,10 @@ CREATE TABLE IF NOT EXISTS `turnero`.`number` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `number` INT(11) NOT NULL,
   `code` VARCHAR(255) NOT NULL,
+  `start` DATETIME NOT NULL,
+  `recalls` INT NULL DEFAULT 0,
+  `end` DATETIME NULL DEFAULT NULL,
   `idPrefix` INT(11) NOT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `called` BINARY(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_number_prefix_idx` (`idPrefix` ASC),
   CONSTRAINT `fk_number_prefix`
@@ -128,9 +126,6 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `turnero`.`call` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `start` DATETIME NOT NULL,
-  `recalls` INT(11) NULL DEFAULT NULL,
-  `end` DATETIME NULL DEFAULT NULL,
   `idNumber` BIGINT(20) NOT NULL,
   `idBox` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -182,13 +177,15 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `turnero`.`users` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `lastName` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `firstName` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
   `password` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
   `remember_token` VARCHAR(100) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `idBox` INT(11) NOT NULL,
+  `deleted_at` TIMESTAMP NULL,
+  `idBox` INT(11) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `users_email_unique` (`email` ASC),
   INDEX `fk_users_box1_idx` (`idBox` ASC),
@@ -200,6 +197,21 @@ CREATE TABLE IF NOT EXISTS `turnero`.`users` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `turnero`.`file`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `turnero`.`file` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `path` VARCHAR(255) NOT NULL,
+  `extension` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NULL,
+  `updated_at` TIMESTAMP NULL,
+  `deleted_at` TIMESTAMP NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
